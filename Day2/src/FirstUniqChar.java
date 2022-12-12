dde03500d54e14aa3fcfea5070af8a8ac09980c2
artyommargaryan <artyom.margaryan.02@gmail.com>
2022-12-12 18:28:09 +0400

    add day 2 problems

---
+++ Day2/src/FirstUniqChar.java
@@ -0,0 +1,15 @@
+import java.util.HashMap;
+
+public class FirstUniqChar {
+    public int firstUniqChar(String s) {
+            int res = Integer.MAX_VALUE;
+            for (char c = 'a'; c <= 'z'; c++) {
+                int ind = s.indexOf(c);
+                if (ind != -1 && ind == s.lastIndexOf(c)) {
+                    res = Math.min(res, ind);
+                }
+            }
+
+            return res == Integer.MAX_VALUE ? -1 : res;
+    }
+}
---
+++ Day2/src/SingleNumber.java
@@ -0,0 +1,10 @@
+public class SingleNumber {
+    public int singleNumber(int[] nums){
+        int result = 0;
+        for (int x : nums) {
+            result ^= x;
+        }
+
+        return result;
+    }
+}
